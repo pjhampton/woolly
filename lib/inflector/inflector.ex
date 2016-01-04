88dbdf430794c1b23c8ad7ac4be5b0231162f3e3
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-01-04 10:26:41 +0000

    Implemented camelize test. Camelize forked from Inflex

--- lib/inflector/inflector.ex
+++ lib/inflector/inflector.ex
@@ -1,4 +1,6 @@
 defmodule Woolly.Inflector do
 
+  defdelegate [camelize(input), camelize(input, :type)], to: Woolly.Inflector.Camelize
+
   defdelegate [parameterize(input), parameterize(input, connector)], to: Woolly.Inflector.Parameterize
 end
--- test/inflector/camelize/camelize_test.exs
+++ test/inflector/camelize/camelize_test.exs
@@ -1,4 +1,20 @@
 defmodule Woolly.Inflector.CamelizeTest do
   use ExUnit.Case, async: true
 
+  import Woolly.Inflector
+
+  test :camelize_upper do
+    assert "AlphaBetaCharlie" == camelize("Alpha Beta Charlie", :upper)
+    assert "ElixirLang" == camelize("elixir lang")
+    assert "MyFairLady" == camelize("My Fair Lady")
+    assert "TextMining" == camelize("Text mining")
+  end
+
+  test :camelize_lower do
+    assert "alphaBetaCharlie" == camelize("Alpha Beta Charlie", :lower)
+    assert "elixirLang" == camelize("elixir lang", :lower)
+    assert "myFairLady" == camelize("My Fair Lady", :lower)
+    assert "textMining" == camelize("Text mining", :lower)
+  end
+
 end
