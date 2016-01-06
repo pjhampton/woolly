3f0ab0719358b0a4aadc844f964d72f968653572
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-01-06 20:57:23 +0000

    added the underscore to the inflector

--- lib/inflector/inflector.ex
+++ lib/inflector/inflector.ex
@@ -3,4 +3,7 @@ defmodule Woolly.Inflector do
   defdelegate [camelize(word), camelize(word, option)], to: Woolly.Inflector.Camelize
 
   defdelegate [parameterize(input), parameterize(input, connector)], to: Woolly.Inflector.Parameterize
+
+  defdelegate [underscore(word)], to: Woolly.Inflector.Underscore
+
 end
