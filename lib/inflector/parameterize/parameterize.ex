a8f404f77eb1d8a2ec28d132a7ba8a43a82395b4
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-01-07 17:59:17 +0000

    removed module doc, will replace at inflector level

--- lib/inflector/parameterize/parameterize.ex
+++ lib/inflector/parameterize/parameterize.ex
@@ -1,10 +1,5 @@
 defmodule Woolly.Inflector.Parameterize do
-  @moduledoc """
-  # Parameterize
-
-  Takes a list of string `input` and concatenates
-  them together using the `connector`.
-  """
+  @moduledoc false
   
   def parameterize(input, connector \\ "-") do
     Regex.split(~r/\s|%20/, input)
