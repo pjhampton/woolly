3f6d7f26c21615ecec79785309f31f84dae965dc
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-01-08 15:43:22 +0000

    added pluralize test

--- test/inflector/pluralize/pluralize_test.exs
+++ test/inflector/pluralize/pluralize_test.exs
@@ -1,4 +1,37 @@
 defmodule Woolly.Inflector.PluralizeTest do
   use ExUnit.Case, async: true
 
+  import Woolly.Inflector
+
+  test :pluralize do
+    assert "dogs" == pluralize("dog")
+    assert "users" == pluralize("user")
+    assert "sheep" == pluralize("sheep")
+    assert "mice" == pluralize("mouse")
+    assert "children" == pluralize("child")
+
+    assert "buses" == pluralize("bus")
+    assert "databases" == pluralize("database")
+    assert "shoes" == pluralize("shoe")
+    assert "viri" == pluralize("virus")
+    assert "geese" == pluralize("goose")
+
+    assert "criteria" == pluralize("criterion")
+    assert "radii" == pluralize("radius")
+    assert "classes" == pluralize("class")
+    assert "women" == pluralize("woman")
+    assert "seamen" == pluralize("seamen")
+
+    assert "men" == pluralize("men")
+    assert "women" == pluralize("women")
+    assert "specimens" == pluralize("specimen")
+    assert "lice" == pluralize("louse")
+    assert "quanta" == pluralize("quantum")
+
+    assert "feet" == pluralize("foot")
+    assert "dice" == pluralize("die")
+    assert "phenomena" == pluralize("phenomenon")
+    assert "buses" == pluralize(:bus)
+  end
+  
 end
