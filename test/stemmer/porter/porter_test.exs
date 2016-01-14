93f5a68e8527fea7adb6395deb2586300b8a2b31
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-01-14 10:22:13 +0000

    Added the 3 remaining stemmer tests

--- test/stemmer/porter/porter_test.exs
+++ test/stemmer/porter/porter_test.exs
@@ -1,4 +1,38 @@
 defmodule Woolly.Stemmer.PorterTest do
   use ExUnit.Case, async: true
 
+  import Woolly.Stemmer.Porter
+
+  # The ground truths for these tests were generated
+  # using the Python nltk - `nltk.stem.porter`
+
+  test :porter_stemmer do
+    assert "caress" = stem("caresses")
+    assert "fli" = stem("flies")
+    assert "die" = stem("dies")
+    assert "mule" = stem("mules")
+    assert "deni" = stem("denied")
+    assert "agre" = stem("agreed")
+    assert "own" = stem("owned")
+    assert "humbl" = stem("humbled")
+    assert "size" = stem("sized")
+    assert "meet" = stem("meeting")
+    assert "state" = stem("stating")
+    assert "sensat" = stem("sensational")
+    assert "tradit" = stem("traditional")
+    assert "refer" = stem("reference")
+    assert "plot" = stem("plotted")
+    assert "discret" = stem("discrete")
+    assert "languag" = stem("language")
+    assert "love" = stem("love")
+    assert "hope" = stem("hope")
+    assert "Elixir" = stem("Elixir")
+    assert "HampTON" = stem("HampTON")
+    assert "Winni" = stem("Winnie")
+    assert "dog" = stem("dog")
+    assert "westi" = stem("westie")
+    assert "comput" = stem("computer")
+    assert "TwiTTer" = stem("TwiTTer")
+    assert "Gener" = stem("Generous")
+  end
 end
