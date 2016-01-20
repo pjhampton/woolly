da715db675c58a1fe78ee60e0375171b1e6e35cf
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-01-20 18:26:24 +0000

    Refactored / Added Documentation to mix.exs.

--- mix.exs
+++ mix.exs
@@ -1,12 +1,16 @@
 defmodule Woolly.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [app: :woolly,
-     version: "0.0.1",
+     name: "Woolly",
+     version: @version,
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     docs: docs,
      deps: deps]
   end
 
@@ -17,5 +21,19 @@ defmodule Woolly.Mixfile do
   defp deps do
     [{:earmark, "~> 0.1", only: :dev},
      {:ex_doc, "~> 0.11", only: :dev}]
+  end
+
+  defp docs do
+    [extras: [
+      "info/CONTRIBUTING.md",
+      "info/FAQs.md",
+      "info/LICENSE.md",
+      ],
+      homepage_url: "http://github.com/pjhampton/woolly",
+      source_url: "http://github.com/pjhampton/woolly",
+      description: """
+        Woolly is a text mining and natural language processing
+        toolkit for the Elixir programming language.
+      """]
   end
 end
