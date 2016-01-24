85c728c63966438b6fd79675723f956670f3f3ea
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-01-24 16:12:10 +0000

    Fixed deps issue in mix.exs. Now Corrected.

--- mix.exs
+++ mix.exs
@@ -20,8 +20,8 @@ defmodule Woolly.Mixfile do
   end
 
   defp deps do
-    [{:earmark, "~> 0.1", only: :docs},
-     {:ex_doc, "~> 0.11", only: :docs}]
+    [{:earmark, "~> 0.1", only: :dev},
+     {:ex_doc, "~> 0.11", only: :dev}]
   end
 
   defp docs do
