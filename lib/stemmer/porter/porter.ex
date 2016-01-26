bbad87f92b2eca1c11949bd660519938b57f0453
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-01-26 17:22:04 +0000

    Step 1 Implemented.

--- lib/stemmer/porter/porter.ex
+++ lib/stemmer/porter/porter.ex
@@ -42,14 +42,185 @@ defmodule Woolly.Stemmer.Porter do
   Stems a token using the Porter Stemming algorithm
 
   """
-  def stem(word) do
+  def stem(word) when byte_size(word) > 2 do
     word
-    # |> step1ab
-    # |> step1c
-    # |> step2
-    # |> step3
-    # |> step4
-    # |> step5
+    |> reverse
+    |> step1
+    |> reverse
+    # |> step2 |> step3 |> step4 |> step5
   end
+
+  def stem(word), do: word
+
+  ##################################################
+  ##################################################
+  ##
+  ## Steps
+  ##
+  ##################################################
+  ##################################################
+
+  # Step1a ~ complete
+  def step1a("sess" <> stem),   do: "ss" <> stem
+  def step1a("sei" <> stem),    do: "i" <> stem
+  def step1a(word = "ss" <> _), do: word
+  def step1a("s" <> stem),      do: stem
+  def step1a(word),             do: word
+
+  # Step1b ~ incomplete
+
+  def step1b(word = "dee" <> stem) do
+    case measure(word) do
+      m when m > 0 ->
+        "ee" <> stem
+      0 ->
+        word
+    end
+  end
+  
+  def step1b(word = "de" <> stem) do
+    case has_vowel(stem) do
+      true  -> false #step1b2(stem) # not implemented
+      false -> word
+    end
+  end
+
+  def step1b(word = "gni" <> stem) do
+    case has_vowel(stem) do
+      true  -> step1b2(stem)
+      false -> word
+    end
+  end
+
+  def step1b(word), do: word
+
+  def step1b2("ta" <> stem), do: "eta" <> stem
+  def step1b2("lb" <> stem), do: "elb" <> stem
+  def step1b2("zi" <> stem), do: "ezi" <> stem
+  
+  def step1b2(stem), do: stem # temp func
+
+
+  #####
+  ##### Step1c
+  #####
+
+  def step1c(word = "y" <> stem) do
+    case has_vowel(stem) do
+      true  -> "i" <> stem
+      false -> word
+    end
+  end
+
+  def step1c(word), do: word
+
+  def step1(word), do: word |> step1a |> step1b |>step1c
+
+  
+
+  ##################################################
+  ##################################################
+  ##
+  ## Conveinance Functions
+  ##
+  ##################################################
+  ##################################################
+
+  defp graphemes(token) do
+    String.graphemes(token)
+  end
+
+  defp reverse(token) when is_binary(token) do
+    token |> String.reverse
+  end
+
+  def is_vowel(letter, y_is_vowel \\ false) do
+    case letter do
+      "a"                 -> true
+      "e"                 -> true
+      "i"                 -> true
+      "o"                 -> true
+      "u"                 -> true
+      "y" when y_is_vowel -> true
+      _                   -> false
+    end
+  end
+ 
+  def measure(word), do: word |> reverse |> measure(0)
+  def measure(word = "", 0), do: 0
+
+  def measure(word, 0) do
+    head = String.first(word)
+    tail = String.slice(word, 1..-1)
+
+    case is_vowel(head) do
+      true  -> found_vowel(tail, 0)
+      false -> found_leading_consonant(tail)
+    end
+  end
+
+  defp found_leading_consonant(word = ""), do: 0
+
+  defp found_leading_consonant(word) do
+    head = String.first(word)
+    tail = String.slice(word, 1..-1)
+    case is_vowel(head, false) do
+      true  -> found_vowel(tail, 0)
+      false -> found_leading_consonant(tail)
+    end
+  end
+
+  defp found_vowel(word = "", m), do: m
+
+  defp found_vowel(word, m) do
+    head = String.first(word)
+    tail = String.slice(word, 1..-1)
+
+    case is_vowel(head, false) do
+      true -> found_vowel(tail, m)
+      false -> found_consonant(tail, m + 1) # not implemented
+    end
+  end
+
+  defp found_consonant(word = "", m), do: 0
+
+  defp found_consonant(word, m) do
+    head = String.first(word)
+    tail = String.slice(word, 1..-1)
+
+    case is_vowel(head, true) do
+      true  -> found_vowel(tail, m)
+      false -> found_consonant(tail, m)
+    end
+  end
+
+  defp has_vowel(word), do: word |> has_vowel1
+
+  defp has_vowel1(word = ""), do: false
+
+  defp has_vowel1(word) do
+    head = String.first(word)
+    tail = String.slice(word, 1..-1)
+
+    case is_vowel(head, false) do
+      true  -> true
+      false -> has_vowel2(tail)
+    end
+  end
+
+  defp has_vowel2(word = ""), do: false
+
+  defp has_vowel2(word) do
+    head = String.first(word)
+    tail = String.slice(word, 1..-1)
+
+    case is_vowel(head, true) do
+      true  -> true
+      false -> has_vowel2(tail)
+    end
+  end
+
+
+
 
 end
