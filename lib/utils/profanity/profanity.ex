6286934b0adb9a20e78441f76fc4ae21236cebc1
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-01-31 18:09:33 +0000

    Set up profanity module source files.

---
+++ lib/utils/profanity/profanity.ex
@@ -0,0 +1,11 @@
+defmodule Woolly.Utils.Profanity do
+  @moduledoc ~S"""
+  A module for analyzing, masking and removing profanity.
+
+  """
+
+  # remove_profanity()
+  # mask_profanity()
+  # list_profanity()
+  # p ~ fetch profanity()
+end
---
+++ test/utils/prof_test.exs
@@ -0,0 +1,6 @@
+defmodule Woolly.Utils.ProfanityTest do
+  use ExUnit.Case, async: true
+
+  import Woolly.Utils.Profanity
+
+end
