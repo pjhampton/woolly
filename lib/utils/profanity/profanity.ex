87305d9caf1a0a1b86e6905440cc61a951133665
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-02-06 15:48:53 +0000

    Adding profanity remover (v0.1.1).

--- lib/utils/profanity/profanity.ex
+++ lib/utils/profanity/profanity.ex
@@ -1,11 +1,55 @@
 defmodule Woolly.Utils.Profanity do
-  @moduledoc ~S"""
-  A module for analyzing, masking and removing profanity.
-
+  @moduledoc """
+  A module for analyzing, removing and replacing profanity
+  within a tokenized list of words.
   """
 
-  # remove_profanity()
-  # mask_profanity()
-  # list_profanity()
-  # p ~ fetch profanity()
+  @doc """
+  Removes stopwords from a list of words.
+
+  # Parameters
+
+    - sent: A tokenized list of words.
+    - lang: An atom representing a language iso 639-1 code.
+  """
+  def remove_profanity(sent, lang \\ :en) do
+    profanity = fetch_profanity(lang)
+    sent |> Enum.reject(fn(x) -> x in profanity end)
+  end
+
+  @doc """
+  Replaces profanity within a tokenized list of words.
+
+  # Parameters
+
+    - sent: A tokenized list of words.
+    - replacer: The String to replace the offending word.
+    - lang: An atom representing a language iso 639-1 code.
+  """
+  def replace_profanity(sent, replacer \\ "", lang \\ :en) do
+    profanity = fetch_profanity(lang)
+    sent |> Enum.map(fn(x) ->
+      if Enum.member?(profanity, x) do replacer else x end
+    end)
+  end
+  
+  @doc """
+  The list_profanity function lists all of the profanity
+  for a specified language. 
+
+  ## Parameters
+ 
+    - lang: An atom representing a language iso 639-1 code.
+  """
+  def list_profanity(lang \\ :en) do
+    lang |> fetch_profanity
+  end
+
+  defp fetch_profanity(lang) do
+    folder_path = "resources/corpora/profanity/"
+    file_path = Atom.to_string(lang) <> ".profanity"
+    path = folder_path <> file_path
+    {:ok, profanity} = File.read(path)
+    Regex.split(~r/\n/, profanity)
+  end
 end
--- test/utils/prof_test.exs
+++ test/utils/prof_test.exs
@@ -3,4 +3,16 @@ defmodule Woolly.Utils.ProfanityTest do
 
   import Woolly.Utils.Profanity
 
+  test :remove_profanity do
+    
+    bad_words1 = ["shit", "m0therfucker", "b00bs", "sh1t"]
+    bad_words2 = ["hello", "love", "fuck", "ass", "a_s_s"]
+    bad_words3 = ["bitch", "ass", "tits", "dick", "cat"]
+    bad_words4 = ["i", "fucking", "hate", "swearing"]
+    
+    assert remove_profanity(bad_words1) == []
+    assert remove_profanity(bad_words2) == ["hello", "love"]
+    assert remove_profanity(bad_words3) == ["cat"]
+    assert remove_profanity(bad_words4) == ["i", "hate", "swearing"]
+  end
 end
