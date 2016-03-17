d3e22c5b18ee15e3ef55f782638b2c6ff314dd84
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-03-17 22:37:34 +0000

    Removed inflector in favour of Johnny Winn's inflex.

--- lib/inflector/parameterize/parameterize.ex
+++
@@ -1,9 +0,0 @@
-defmodule Woolly.Inflector.Parameterize do
-  @moduledoc false
-  
-  def parameterize(input, sep \\ "-") do
-    Regex.split(~r/\s|%20/, input)
-    |> Enum.join(sep)
-    |> String.downcase()
-  end
-end
